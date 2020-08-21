13c8cd4877204fb252eb5b1985721d1b53f4a051
fernandoWH <59174289+fernandoWH@users.noreply.github.com>
2020-08-21 12:44:46 -0500

    Update settings.py

--- monitoreo/monitoreo/settings.py
+++ monitoreo/monitoreo/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wg+u(i)ms!#sinr!^xoj+el1pb!f4glt8g%(5iek(ilh114#05'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
